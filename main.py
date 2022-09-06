c88a6155b02678fa0a561751fe08320f141394e7
Rafael Cunha <rafacunha.prof033@gmail.com>
2022-09-06 23:06:14 +0000

    ists, concatenation and conditional

---
+++ main.py
@@ -0,0 +1,21 @@
+# Comments
+the_world_is_flat = True
+if the_world_is_flat:
+    print("Be careful not to fall off!")
+
+squares = [1 ,2, 3]
+squares.append(7)
+print(squares)
+
+text = '1'
+text2 = '2'
+
+# Concatenation:
+print(text + text2)
+
+# If-else
+variable = 0
+if variable:
+    print('True')
+else:
+    print('False')
