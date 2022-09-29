a706358e2603beb94087685dd296c6da27040361
Rafael Cunha <rafacunha.prof033@gmail.com>
2022-09-29 23:20:28 +0000

    Variables

--- examples/variables.py
+++ examples/variables.py
@@ -1,3 +1,10 @@
 name = "Rafael"
+nameType = type(name)
 
-print(name)
+## Concatenation
+# Using "," it's not necessary to use space
+print(name, nameType)
+
+# When using the + operator it's necessary to use space
+# Also can't concatenate str with type or anything else
+print("Hello "+name) 
