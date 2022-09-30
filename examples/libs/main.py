b70e460ef37a2d4087fef5a3f24c4a661c8683a7
Rafael Cunha <rafacunha.prof033@gmail.com>
2022-09-30 00:57:18 +0000

    Pandas dataframe

--- examples/libs/main.py
+++ examples/libs/main.py
@@ -5,4 +5,14 @@ elements = np.random.randint(0, 10, 15)
 
 print(elements)
 
+user1 = ["John", 15, 1.80]
+user2 = ["Abraham", 35, 1.80]
+user3 = ["WD", 23, 1.80]
 
+userClass = [user1, user2, user3]
+
+userColumns = ["name", "age", "height"]
+userLines = ["#1", "#2", "#3"]
+
+df = pd.DataFrame(data=userClass, index=userLines, columns=userColumns)
+print(df)
